730a1e1a1955346aee937ba7f822a31c57ac9a05
Niklas Blomqvist <nikbl918@zaza7.ida.liu.se>
2013-04-24 17:01:52 +0200

    Lab 3 serious progress

--- lab3/serial.asm
+++ lab3/serial.asm
@@ -12,6 +12,7 @@ LSR_PORT	EQU	BASE_PORT+5
 RBR_PORT	EQU	BASE_PORT
 THR_PORT	EQU	BASE_PORT
 
+
 ; ---------------------------------------------------------------------
 ; void SerialPut(char ch)
 ; ---------------------------------------------------------------------
@@ -19,10 +20,16 @@ THR_PORT	EQU	BASE_PORT
 ; to output the ASCII character 'ch' to the UART.
 
 		GLOBAL	SerialPut
-
-SerialPut:	; <your code here>	; (1) Wait for THRE = 1
-		; <your code here>	; (2) Output character to UART
-		; <your code here>	; (3) Return to caller
+SerialPut:
+		MOV AX, [LSR_PORT]	; (1) Wait for THRE = 1
+		IN AX, DX		; AX -> DX
+		AND DX, 0x20		; Mask that shit piece out
+		TEST DX, 0
+		JZ SerialPut		; Equal zero yes? then goto backtto
+		MOV DX, [THR_PORT]
+		MOV AX, [EBP + 4]
+		OUT DX, AX     		;
+		RET			; Yolo_return
 
 ; ---------------------------------------------------------------------
 ; void interrupt SerialISR(void)
@@ -34,16 +41,27 @@ SerialPut:	; <your code here>	; (1) Wait for THRE = 1
 		GLOBAL	SerialISR
 		EXTERN	QueueInsert	; (provided by LIBPC)
 
-SerialISR:	; <your code here>	; (1) Enable (higher-priority) IRQs 
-		; <your code here>	; (2) Preserve all registers 
-		; <your code here>	; (3) Get character from UART
-		; <your code here>	; (4) Put character into queue 
-		; <your code here>	; Param #2: address of data
-		; <your code here>	; Param #1: address of queue
+SerialISR:	STI			; (1) Enable (higher-priority) IRQs 
+		PUSHA			; (2) Preserve all registers 
+
+		MOV AX, [LSR_PORT]	; (3) Get character from UART
+		IN AX, DX
+		AND DX, 0x01		; See if read buffer is full
+		TEST DX, 0
+		JZ _Eoi		  	; We got no data (false interrupt)
+
+		MOV AX, [RBR_PORT] 	; Fetch data
+		IN AX, DX
+		MOV [EBP + 4], DX	; (4) Put character into queue 
+					; Param #2: address of data
+					; Param #1: address of queue
 		CALL	QueueInsert
 		ADD	ESP,8
 
-_Eoi:		; <your code here>	; (5) Enable lower priority interrupts
-		; <your code here>	;       (Send Non-Specific EOI to PIC)
-		; <your code here>	; (6) Restore all registers
-		; <your code here>	; (7) Return to interrupted code
+_Eoi:		MOV DX, 0x20		; (5) Enable lower priority interrupts
+		MOV AX, 0		;       (Send Non-Specific EOI to PIC)
+		OUT DX, AX
+		POPA		; (6) Restore all registers
+	
+
+		RET			; (7) Return to interrupted code
